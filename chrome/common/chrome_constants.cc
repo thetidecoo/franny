RebelBrowser/rebel

Branding: Minimum changes to compile with Rebel branding on macOS

chrome/common/chrome_constants.cc
@@ -14,6 +14,9 @@
 #if BUILDFLAG(IS_MAC)
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #define PRODUCT_STRING "Google Chrome"
+#elif BUILDFLAG(REBEL_BRANDING)
+#include "rebel/build/buildflag.h"
+#define PRODUCT_STRING REBEL_STRING_BUILDFLAG(REBEL_BROWSER_NAME)
 #elif BUILDFLAG(CHROMIUM_BRANDING)
 #define PRODUCT_STRING "Chromium"
 #else

thetidecoo/franny

chrome/common/chrome_constants.cc
@@ -15,6 +15,9 @@
 #define PRODUCT_STRING "Google Chrome"
 #elif BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
 #define PRODUCT_STRING "Google Chrome for Testing"
+#elif BUILDFLAG(REBEL_BROWSER)
+#include "rebel/build/buildflag.h"
+#define PRODUCT_STRING REBEL_STRING_BUILDFLAG(REBEL_BROWSER_NAME)
 #elif BUILDFLAG(CHROMIUM_BRANDING)
 #define PRODUCT_STRING "Chromium"
 #else