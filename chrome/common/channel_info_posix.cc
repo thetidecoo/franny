RebelBrowser/rebel

Branding: Minimum changes to compile with Rebel branding on Linux

chrome/common/channel_info_posix.cc
@@ -17,6 +17,10 @@
 #include "build/chromeos_buildflags.h"
 #include "components/version_info/version_info.h"
 
+#if BUILDFLAG(REBEL_BROWSER)
+#include "rebel/build/buildflag.h"
+#endif
+
 namespace chrome {
 
 namespace {
@@ -113,6 +117,8 @@ std::string GetDesktopName(base::Environment* env) {
       // Extended stable is not differentiated from regular stable.
       return "google-chrome.desktop";
   }
+#elif BUILDFLAG(REBEL_BROWSER)
+  return REBEL_STRING_BUILDFLAG(REBEL_BROWSER_NAME_PATH) ".desktop";
 #else  // BUILDFLAG(CHROMIUM_BRANDING)
   // Allow $CHROME_DESKTOP to override the built-in value, so that development
   // versions can set themselves as the default without interfering with

chrome/common/chrome_paths_linux.cc
@@ -91,6 +91,8 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   std::string data_dir_basename = "google-chrome";
+#elif BUILDFLAG(REBEL_BROWSER)
+  std::string data_dir_basename = BUILDFLAG(REBEL_BROWSER_NAME_PATH);
 #else
   std::string data_dir_basename = "chromium";
 #endif

thetidecoo/franny

chrome/common/channel_info_posix.cc
@@ -18,6 +18,10 @@
 #include "build/chromeos_buildflags.h"
 #include "components/version_info/version_info.h"
 
+#if BUILDFLAG(REBEL_BROWSER)
+#include "rebel/build/buildflag.h"
+#endif
+
 namespace chrome {
 
 namespace {
@@ -138,6 +142,8 @@ std::string GetDesktopName(base::Environment* env) {
       // Extended stable is not differentiated from regular stable.
       return "google-chrome.desktop";
   }
+#elif BUILDFLAG(REBEL_BROWSER)
+  return REBEL_STRING_BUILDFLAG(REBEL_BROWSER_NAME) ".desktop";
 #else  // BUILDFLAG(CHROMIUM_BRANDING)
   // Allow $CHROME_DESKTOP to override the built-in value, so that development
   // versions can set themselves as the default without interfering with

chrome/common/chrome_paths_linux.cc
@@ -93,6 +93,8 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
   std::string data_dir_basename = "google-chrome-for-testing";
 #elif BUILDFLAG(GOOGLE_CHROME_BRANDING)
   std::string data_dir_basename = "google-chrome";
+#elif BUILDFLAG(REBEL_BROWSER)
+  std::string data_dir_basename = BUILDFLAG(REBEL_BROWSER_NAME);
 #else
   std::string data_dir_basename = "chromium";
 #endif